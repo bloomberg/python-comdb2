d2738d65f637330f6880965b0371adab95be02ed
Alex Chamberlain <achamberlai9@bloomberg.net>
2016-08-11 18:06:17 +0100

    Fixup build and naming scheme.
    Signed-off-by: Alex Chamberlain <achamberlai9@bloomberg.net>

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='python-cdb2api',
+    name='python-comdb2',
     version='1.0',
     author='Alex Chamberlain',
     author_email='achamberlai9@bloomberg.net',
-    packages=['cdb2api'],
+    packages=['comdb2'],
     setup_requires=["cffi>=1.0.0"],
     cffi_modules=["build.py:ffi"],
     install_requires=["cffi>=1.0.0", "six", "pytz"]
