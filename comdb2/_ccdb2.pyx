dd37705fee5bf4eee73cc97dfa0c552fb11480a1
Matt Wozniski <mwozniski@bloomberg.net>
2017-08-15 21:30:52 -0400

    Add copyright comments to all new files

--- comdb2/_ccdb2.pyx
+++ comdb2/_ccdb2.pyx
@@ -1,4 +1,16 @@
 # cython: c_string_type=str, c_string_encoding=utf-8
+
+# Copyright 2017 Bloomberg Finance L.P.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Thin Python wrapper over libcdb2api"""
 
 from libc.string cimport strcmp, strcpy
