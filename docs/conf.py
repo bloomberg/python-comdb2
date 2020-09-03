750a8c90c094b722ae877013321269c34b37a477
Matt Wozniski <mwozniski@bloomberg.net>
2020-09-03 15:54:26 -0400

    docs: Begin using sphinx githubpages extension
    This extension automates creating of the `.nojekyll` file, so that the
    generated documentation renders properly on GitHub Pages.
    
    Signed-off-by: Matt Wozniski <mwozniski@bloomberg.net>

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
