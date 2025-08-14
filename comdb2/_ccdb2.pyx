0e9c4e8493169c8701c24af04f678dfe18a77cfd
Matt Wozniski <mwozniski@bloomberg.net>
2025-08-14 18:12:20 -0400

    Stop using pytz from Python 3.14 onwards
    I'm a bit nervous about pulling the trigger on getting rid of `pytz`
    entirely, so this is a baby step in the right direction: starting from
    Python 3.14, use `zoneinfo` to create the time zone objects that we
    attach to the `datetime` objects fetched by queries, rather than `pytz`.
    
    Signed-off-by: Matt Wozniski <mwozniski@bloomberg.net>

--- comdb2/_ccdb2.pyx
+++ comdb2/_ccdb2.pyx
@@ -20,8 +20,7 @@ from cpython.ref cimport Py_TYPE, PyObject
 from cpython.mem cimport PyMem_Malloc, PyMem_Free
 
 import datetime
-
-from pytz import timezone, UTC
+import sys
 
 from ._cdb2_types import Error, Effects, DatetimeUs
 from . cimport _cdb2api as lib
@@ -266,11 +265,13 @@ cdef _make_datetime(client_datetime *val):
                     val.tm.tm_min,
                     val.tm.tm_sec,
                     usec,
-                    UTC,
+                    datetime.timezone.utc,
                     pytype)
 
-    return timezone(val.tzname).localize(
-        PyDateTimeAPI.DateTime_FromDateAndTime(
+    if sys.version_info >= (3, 14):
+        from zoneinfo import ZoneInfo
+
+        return PyDateTimeAPI.DateTime_FromDateAndTimeAndFold(
                     val.tm.tm_year + 1900,
                     val.tm.tm_mon + 1,
                     val.tm.tm_mday,
@@ -278,9 +279,24 @@ cdef _make_datetime(client_datetime *val):
                     val.tm.tm_min,
                     val.tm.tm_sec,
                     usec,
-                    None,
-                    pytype),
-        val.tm.tm_isdst)
+                    ZoneInfo(val.tzname),
+                    not val.tm.tm_isdst,
+                    pytype)
+    else:
+        from pytz import timezone
+
+        return timezone(val.tzname).localize(
+            PyDateTimeAPI.DateTime_FromDateAndTime(
+                        val.tm.tm_year + 1900,
+                        val.tm.tm_mon + 1,
+                        val.tm.tm_mday,
+                        val.tm.tm_hour,
+                        val.tm.tm_min,
+                        val.tm.tm_sec,
+                        usec,
+                        None,
+                        pytype),
+            val.tm.tm_isdst)
 
 
 cdef _column_value(lib.cdb2_hndl_tp *hndl, int col):
